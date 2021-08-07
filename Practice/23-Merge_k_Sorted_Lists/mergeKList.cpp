1fdcdab44c53cf14b8cbec7e33d423d609887025
Aman Singh <42384948+AmanSingh0-0@users.noreply.github.com>
2021-08-07 16:27:34 +0530

    Create mergeKList.cpp

---
+++ Practice/23-Merge_k_Sorted_Lists/mergeKList.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode clone(INT_MAX);
+        ListNode *temp = &clone;
+        while(l1 && l2){
+            if(l1->val<l2->val){
+                temp->next = l1;
+                l1 = l1->next;
+            }else{
+                temp->next = l2;
+                l2 = l2->next;
+            }
+            temp = temp->next;
+        }
+        temp->next = l1 ? l1 : l2;
+        return clone.next;
+    }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.empty()){
+            return nullptr;
+        }
+        while(lists.size()!=1){
+            lists.push_back(mergeTwoLists(lists[0], lists[1]));
+            lists.erase(lists.begin());
+            lists.erase(lists.begin());
+        }
+        return lists.front();
+    }
+};
